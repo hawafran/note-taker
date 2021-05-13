1071ec2d377e527c9efa29fa840e54b9c7558a3a
Hawa <hawa.francis@gmail.com>
2021-05-13 05:13:01 -0400

    server.js

--- Develop/routes/html*routes.js
+++ Develop/routes/html*routes.js
@@ -2,12 +2,15 @@
 const path = require('path');
 
 module.exports = app => {
+
     
-    app.get('/notes', (req, res) => 
+    
+    app.get("/notes", (req, res) => 
         res.sendFile(path.join(__dirname, './public/notes.html')));
+        
     app.get('*', (req, res) => 
-        res.sendFile(path.join(__dirname, '/public/index.html')));
+        res.sendFile(path.join(__dirname, './public/index.html')));
    
 
-}
+};
     
