c6d77d24ba1b260efe1014370a237ab740677f75
Hawa <hawa.francis@gmail.com>
2021-06-25 10:07:23 -0700

    static

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 // ROUTES
 require('./routes/apiRoutes')(app);
